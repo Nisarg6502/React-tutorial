b778ec2bbe57bce072e4e2d90f1eadac5b946db9
Nisarg6502 <nisarg6502@gmail.com>
2023-01-31 19:24:37 +0530

    named export updated

--- JavaScript Brush-up/app.js
+++ JavaScript Brush-up/app.js
@@ -4,6 +4,9 @@ import person from "./person";
 
 import { baseData } from "./utility";
 import { clean } from "./utility";
+// import { clean as Clean } from "./utility";
+// import * as bundled from './utility.js'
+//These are the examples of named exports
 
 console.log(person.name)
 console.log(clean)
