14b31b3750fc63291e5ce6f032b2970c78f0c85f
Nisarg6502 <nisarg6502@gmail.com>
2023-01-31 18:58:14 +0530

    arrowFunction added

---
+++ JavaScript Brush-up/arrowFunctions.js
@@ -0,0 +1,11 @@
+function printMyName(name) {
+    console.log(name)
+}
+
+printMyName('Nisarg')
+
+const printName = (Name) => { //omit the brackets if there is only one argument
+    console.log(Name)
+}
+
+printName('Nisarg')
