36ee899514de197c3416af73a502d092b730fc57
Nisarg6502 <nisarg6502@gmail.com>
2023-02-04 23:30:04 +0530

    Destructuring done

---
+++ JavaScript Brush-up/destructuring.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3];
+[num1, , num3] = numbers;
+console.log(num1, num3);
+
+//To illustrate destructuring, we'll make a sandwich. Do you take everything out of the refrigerator to make your sandwich? No, you only take out the items you would like to use on your sandwich.
+
+//Destructuring is exactly the same.We may have an array or object that we are working with, but we only need some of the items contained in these.
+
+//Destructuring makes it easy to extract only what is needed.
