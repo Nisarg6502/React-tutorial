ec3cb9d7568166505967acb18a6683e5d232537e
Nisarg6502 <nisarg6502@gmail.com>
2023-02-10 23:04:13 +0530

    state introduction and useState done

--- react-complete-guide/src/components/Expenses/ExpenseItems.js
+++ react-complete-guide/src/components/Expenses/ExpenseItems.js
@@ -1,6 +1,7 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css';
 import Card from '../UI/Card';
+import React, { useState } from 'react'
 
 const ExpenseItem = (props) => {
     // const expenseDate = new Date(2022, 4, 10);
@@ -9,15 +10,32 @@ const ExpenseItem = (props) => {
 
     // <div>{props.date.toLocaleString('en-us', { month: 'long' })}</div> not a clean code
 
+    const [title, setTitle] = useState(props.title)
+    /*
+        The React useState Hook allows us to track state in a function component.
+        State generally refers to data or properties that need to be tracking in an application.
 
+        useState accepts an initial state and returns two values:
+        --->The current state.
+        --->A function that updates the state.
+
+        A separate state is called each time it is called and these states are managed behind the scenes by React. 
+    */
+
+    const clickHandler = () => {
+        //title = 'Updated!' ---> Doesn't actually update on the screen but updates the variable in memory
+        setTitle('Updated!') //works because this causes the component to be re-evaluated and hence during re-evaluation, the newly assigned value is picked up.
+        console.log(title)
+    }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate> {/*props are used to send data to child nodes*/}
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
